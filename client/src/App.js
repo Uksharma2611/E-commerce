541632dae91f02e2ac5477d5217ce1e6883e14de
uksharma <mysrms2021@gmail.com>
2024-08-02 11:39:37 +0530

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
-import {Routes,Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import NotFound from './pages/Pagenotfound';
+import PrivacyPolicy from './pages/Policy'; // Add this import
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<PrivacyPolicy />} /> 
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
