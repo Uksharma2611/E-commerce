5b797facbb6e6938dc69225780f8cafe51461467
uksharma <mysrms2021@gmail.com>
2024-08-04 19:21:31 +0530

    completed product api

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
-import authRoutes from './routes/authRoute.js'
-import categoryRoutes from "./routes/categoryRoutes.js"
-import cors from "cors"
+import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cors from "cors";
 
 //configure env
 dotenv.config();
@@ -17,13 +18,14 @@ connectDB();
 const app = express();
 
 //middleware
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev")); // Fixed the missing parenthesis here
 
 //routes
-app.use('/api/v1/auth',authRoutes)
-app.use('/api/v1/category',categoryRoutes)
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product",productRoutes)
 
 //rest api
 app.get("/", (req, res) => {
