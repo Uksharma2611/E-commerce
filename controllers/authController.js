af0db844769a0caf517d0841f7fe4e29662408eb
uksharma <mysrms2021@gmail.com>
2024-08-09 10:22:42 +0530

    done user profile

--- controllers/authController.js
+++ controllers/authController.js
@@ -161,3 +161,38 @@ export const forgotPasswordController = async (req, res) => {
 export const testController = (req, res) => {
   res.send("Protected Route");
 };
+
+//update prfole
+export const updateProfileController = async (req, res) => {
+  try {
+    const { name, email, password, address, phone } = req.body;
+    const user = await userModel.findById(req.user._id);
+    //password
+    if (!password) {
+      return res.json({ error: "Passsword is required " });
+    }
+    const hashedPassword = password ? await hashPassword(password) : undefined;
+    const updatedUser = await userModel.findByIdAndUpdate(
+      req.user._id,
+      {
+        name: name || user.name,
+        password: hashedPassword || user.password,
+        phone: phone || user.phone,
+        address: address || user.address,
+      },
+      { new: true }
+    );
+    res.status(200).send({
+      success: true,
+      message: "Profile Updated SUccessfully",
+      updatedUser,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      success: false,
+      message: "Error While Update profile",
+      error,
+    });
+  }
+};
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -4,6 +4,7 @@ import {
   loginController,
   testController,
   forgotPasswordController,
+  updateProfileController,
 } from "../controllers/authController.js";
 import {isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 
@@ -32,6 +33,8 @@ router.get("/admin-auth",requireSignIn,isAdmin,(req,res)=>{
   res.status(200).send({ok: true});
 })
 
+//update profile
+router.put("/profile", requireSignIn, updateProfileController);
 
 
 export default router;
