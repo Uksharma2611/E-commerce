af3960e26dc384e584717ffc434506de8a021b81
uksharma <mysrms2021@gmail.com>
2024-08-09 19:02:16 +0530

    done complete non css

--- client/src/components/Layout/AdminMenu.js
+++ client/src/components/Layout/AdminMenu.js
@@ -15,6 +15,8 @@ const AdminMenu = () => {
           </NavLink>
           <NavLink to="/dashboard/admin/products" className="list-group-item list-group-item-action">
             Products
+          </NavLink><NavLink to="/dashboard/admin/orders" className="list-group-item list-group-item-action">
+            Orders
           </NavLink>
           <NavLink to="/dashboard/admin/users" className="list-group-item list-group-item-action">
             Users
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -5,8 +5,11 @@ import {
   testController,
   forgotPasswordController,
   updateProfileController,
+  getOrdersController,
+  getAllOrdersController,
+  orderStatusController,
 } from "../controllers/authController.js";
-import {isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 
 //router object
 const router = express.Router();
@@ -22,19 +25,32 @@ router.post("/login", loginController);
 router.post("/forgot-password", forgotPasswordController);
 
 //test routes
-router.get('/test',requireSignIn,isAdmin, testController)
+router.get("/test", requireSignIn, isAdmin, testController);
 
 //protected User route auth
-router.get("/user-auth",requireSignIn,(req,res)=>{
-  res.status(200).send({ok: true});
-})
+router.get("/user-auth", requireSignIn, (req, res) => {
+  res.status(200).send({ ok: true });
+});
 //protected Admin route auth
-router.get("/admin-auth",requireSignIn,isAdmin,(req,res)=>{
-  res.status(200).send({ok: true});
-})
+router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
+  res.status(200).send({ ok: true });
+});
 
 //update profile
 router.put("/profile", requireSignIn, updateProfileController);
 
+//orders
+router.get("/orders", requireSignIn, getOrdersController);
+
+//all orders
+router.get("/all-orders", requireSignIn, isAdmin, getAllOrdersController);
+
+// order status update
+router.put(
+  "/order-status/:orderId",
+  requireSignIn,
+  isAdmin,
+  orderStatusController
+);
 
 export default router;
